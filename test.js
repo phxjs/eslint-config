a86ae2efb326c531a5e7d3648922e6dbac700f03
Bryan Maamo <ninbryan@users.noreply.github.com>
2024-04-10 03:29:46 -0700

    Update test.js

--- test.js
+++ test.js
@@ -6,8 +6,26 @@ const test = require('tape');
 const overrideConfigFile = require.resolve('./eslintrc.json');
 const linter = new ESLint({ overrideConfigFile });
 
-test('api: lintText', async t => {
+test('api: lintText semicolon', async (t) => {
   t.plan(1);
   const [result] = await linter.lintText("console.log('hi there')\n\n");
   t.equal(result.messages[0].message, 'Missing semicolon.');
+});
+
+test('api: lintText arrow-parens', async (t) => {
+  t.plan(1);
+  const [result] = await linter.lintText("const fn = a => console.log(a); fn('yo');\n");
+  t.equal(result.messages[0].message, 'Expected parentheses around arrow function argument.');
+});
+
+test('api: lintText named function spacing', async (t) => {
+  t.plan(1);
+  const [result] = await linter.lintText("namedFn(); function namedFn () { console.log('a named function'); }\n\n");
+  t.equal(result.messages[0].message, 'Unexpected space before function parentheses.');
+});
+
+test('api: lintText variable function spacing', async (t) => {
+  t.plan(1);
+  const [result] = await linter.lintText("const arwFn = () => console.log('anon function'); arwFn ();\n");
+  t.equal(result.messages[0].message, 'Unexpected whitespace between function name and paren.');
 });
