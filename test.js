3f25e67000ae0db107291af0c396f93707e731e0
Bryan Maamo <ninbryan@users.noreply.github.com>
2024-04-10 02:45:24 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const { ESLint } = require('eslint');
+const test = require('tape');
+
+const overrideConfigFile = require.resolve('./eslintrc.json');
+const linter = new ESLint({ overrideConfigFile });
+
+test('api: lintText', async t => {
+  t.plan(1);
+  const [result] = await linter.lintText("console.log('hi there')\n\n");
+  t.equal(result.messages[0].message, 'Missing semicolon.');
+});
